d5cd441c9d927f506c659ebad0c81b8d2ec5c25b
Joseph Mathew <167978292+Josephmathew072@users.noreply.github.com>
2024-09-23 04:26:57 +0530

    Update orderModel.js

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -27,10 +27,10 @@ const orderSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
-//     cod: {
-//       type: Boolean,
-//       default: false,
-//     },
+    cod: {
+      type: Boolean,
+      default: false,
+    },
     
   },
 //   { timestamps: true }
